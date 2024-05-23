8c8e7f52e1ac8995ce51098457d53ccccd70682e
Sumukh Aradhya K J <56041607+Sumukharadhya@users.noreply.github.com>
2024-05-23 13:46:29 +0000

    conversion_operator

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,21 @@
+
+let score = "sumukh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+let isLoggedIn = "dumn"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(valueInNumber)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
