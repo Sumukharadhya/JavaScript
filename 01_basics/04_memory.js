28196b3cf68db3017bfb80189a598f1641298492
Sumukh Aradhya K J <56041607+Sumukharadhya@users.noreply.github.com>
2024-05-24 18:07:45 +0000

    Stack and Heap memory.

---
+++ 01_basics/04_memory.js
@@ -0,0 +1,21 @@
+let myYoutubename = "Sumukhdotcom"
+
+let anothername = "Sumukhdotcom"
+anothername = "kjdotcom"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+
+let userOne = {
+    email: "user@gogle.com",
+     upi : "user@sbi"
+}
+
+let userTwo = userOne
+
+userTwo.upi = "kj@sbi"
+userTwo = "kj@google.com"
+
+console.log(userOne.upi);
+console.log(userTwo.email);
