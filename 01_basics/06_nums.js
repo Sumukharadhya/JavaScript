ee667440240a1eeae0e37dc1068fd8c792eb9bbb
Sumukh Aradhya K J <56041607+Sumukharadhya@users.noreply.github.com>
2024-05-27 02:15:45 +0000

    numbers

---
+++ 01_basics/06_nums.js
@@ -0,0 +1,17 @@
+
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+const othernumber = 123.8966
+
+console.log(othernumber.toPrecision(3)); 
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
