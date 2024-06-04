3d089d9795b0f3c0fb45c62ec5b109570a21309b
Sumukh Aradhya K J <56041607+Sumukharadhya@users.noreply.github.com>
2024-06-04 17:34:18 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -35,4 +35,33 @@ function loginUserMessage(username){
     }
     return `${username} just logged in`
 }
-console.log(loginUserMessage("Sumukh"));
+//console.log(loginUserMessage("Sumukh"));
+
+function calculateCartPrice(val1,val2, ...num1) {
+return num1
+
+}
+
+//console.log(calculateCartPrice(200,400, 700));
+
+const user = {
+    username: "Sumukh",
+    price: 199
+}
+/*
+function handleObject(anyobject){
+console.log(`user name is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject({
+    username:"kj",
+    price: 200
+})
+*/
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[2]
+
+}
+console.log(returnSecondValue(myNewArray));
