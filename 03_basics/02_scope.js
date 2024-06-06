d3805fe9c4ddabe4a3a37ee4241c296b72d2d1fd
Sumukh Aradhya K J <56041607+Sumukharadhya@users.noreply.github.com>
2024-06-06 17:08:22 +0000

    functions

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,4 +1,4 @@
-
+/*
 let a = 100
 if (true) {
     let a = 10
@@ -14,3 +14,27 @@ for (let i =0; i< array.length; i++){
 console.log(a);
 //console.log(b);
 //console.log(c);
+
+*/
+function one(){
+    const username = "Sumukh"
+
+    function two() {
+        const website = "google"
+        console.log(username);
+    }
+   // console.log(website);
+
+    // two()
+}
+
+// one()
+
+if (true) {
+    const username = "Sumukh"
+    if (username == "Sumukh"){
+        const website = " google"
+        console.log(username + website);
+}
+}
+
