f0a0ea3c7b2e0e3fc4de3618098898c616797d35
Sumukh Aradhya K J <56041607+Sumukharadhya@users.noreply.github.com>
2024-06-13 16:57:09 +0000

    map

--- 05_iteration/three.js
+++ 05_iteration/three.js
@@ -1,5 +1,20 @@
 const arr = [1,2,3,4,5]
 
-for (const interator of object) {
-    
+for (const num of arr) {
+    //  console.log(num);
 }
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+// console.log('Each cahr is ${`greet}')
+}
+
+// maps
+
+const map = new Map()
+map.set("IN","INDIA")
+map.set("USA","UNITED STATES OF AMERICA")
+map.set("FR","FRANCE")
+
+console.log(map);
+
